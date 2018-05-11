43f2f65d6c8220fb6d58d8b3e8ec05982d83ebe2
Feliciano Long <noahzark@vip.qq.com>
2018-05-11 09:54:28 -0500

    inc version

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from controller.config_file_handler import YamlConfigFileHandler
 
 from init import report_logger
 
-GRADER_VERSION = '1.2.1'
+GRADER_VERSION = '1.2.2'
 
 if __name__ == '__main__':
     report_logger.info("QiwuGrader ver {0}".format(GRADER_VERSION))
