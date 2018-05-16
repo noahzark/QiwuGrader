7c764e912ada074a9478e1bcba7a592d6602937f
Feliciano Long <noahzark@vip.qq.com>
2018-05-16 15:33:19 -0500

    add loop parameter in grader skeleton and bypass parameter dict in thread/process constructors

--- grader_thread.py
+++ grader_thread.py
@@ -6,11 +6,12 @@ from grader import Grader
 
 class GraderSkeleton:
 
-    def __init__(self, shared_counter, test_config):
+    def __init__(self, shared_counter, test_config, loop=1):
         self.grader = Grader()
 
         self.shared_counter = shared_counter
         self.test_config = test_config
+        self.loop = 1
 
     def init(self):
         self.grader.init(self.test_config)
@@ -20,18 +21,20 @@ class GraderSkeleton:
             self.shared_counter.increment()
 
     def run(self):
-        self.grade()
+        while self.loop > 0:
+            self.grade()
+            self.loop -= 1
 
 
 class GraderThread(GraderSkeleton, threading.Thread):
 
-    def __init__(self, shared_counter, test_config):
-        super(GraderThread, self).__init__(shared_counter, test_config)
+    def __init__(self, shared_counter, test_config, **kwargs):
+        super(GraderThread, self).__init__(shared_counter, test_config, kwargs)
         threading.Thread.__init__(self)
 
 
 class GraderProcess(GraderSkeleton, multiprocessing.Process):
 
-    def __init__(self, shared_counter, test_config):
-        super(GraderProcess, self).__init__(shared_counter, test_config)
+    def __init__(self, shared_counter, test_config, **kwargs):
+        super(GraderProcess, self).__init__(shared_counter, test_config, kwargs)
         multiprocessing.Process.__init__(self)
