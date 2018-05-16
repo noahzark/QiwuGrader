16b8a704c4579a24af0f8b8456709a5b9e10024d
Feliciano Long <noahzark@vip.qq.com>
2018-05-16 15:17:31 -0500

    refactor init and put log file output in a function

--- init.py
+++ init.py
@@ -10,36 +10,18 @@ logging.basicConfig(
     format=FORMAT,
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.sys.path.insert(0, parent_dir)
-
-logPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), './logs')
-if not os.path.exists(logPath):
-    os.makedirs(logPath)
 
 # init report logger
-GRADE_REPORT_FILENAME = './logs/QiwuGrade.log'
-logPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), GRADE_REPORT_FILENAME)
-report_logHandler = logging.handlers.RotatingFileHandler(logPath, backupCount=50, encoding=file_encoding)
 report_logger = logging.getLogger("GradeReport")
 report_logger.setLevel(logging.INFO)
-report_logger.addHandler(report_logHandler)
 
 # init test logger
-GRADE_LOG_FILENAME = './logs/QiwuTest.log'
-logPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), GRADE_LOG_FILENAME)
-test_logHandler = logging.handlers.RotatingFileHandler(logPath, backupCount=50, encoding=file_encoding)
 test_logger = logging.getLogger("TestLog")
 test_logger.setLevel(logging.INFO)
-test_logger.addHandler(test_logHandler)
 
 # init csv logger
-CSV_LOG_FILENAME = './logs/QiwuTest.csv'
-logPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), CSV_LOG_FILENAME)
-csv_logHandler = logging.handlers.RotatingFileHandler(logPath, backupCount=50, encoding=file_encoding)
 csv_logger = logging.getLogger("CsvLog")
 csv_logger.setLevel(logging.INFO)
-csv_logger.addHandler(csv_logHandler)
 
 # disable library logging
 urllib3_logger = logging.getLogger('urllib3')
@@ -47,9 +29,33 @@ urllib3_logger.setLevel(logging.CRITICAL)
 requests_logger = logging.getLogger('requests')
 requests_logger.setLevel(logging.WARNING)
 
-# do roll log file
-needRoll = os.path.isfile(logPath)
-if needRoll:
-    report_logHandler.doRollover()
-    test_logHandler.doRollover()
-    csv_logHandler.doRollover()
+
+def init_log_file():
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.sys.path.insert(0, parent_dir)
+
+    log_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), './logs')
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    grade_report_filename = './logs/QiwuGrade.log'
+    log_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), grade_report_filename)
+    report_log_handler = logging.handlers.RotatingFileHandler(log_path, backupCount=50, encoding=file_encoding)
+    report_logger.addHandler(report_log_handler)
+
+    grade_log_filename = './logs/QiwuTest.log'
+    log_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), grade_log_filename)
+    test_log_handler = logging.handlers.RotatingFileHandler(log_path, backupCount=50, encoding=file_encoding)
+    test_logger.addHandler(test_log_handler)
+
+    csv_log_filename = './logs/QiwuTest.csv'
+    log_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), csv_log_filename)
+    csv_log_handler = logging.handlers.RotatingFileHandler(log_path, backupCount=50, encoding=file_encoding)
+    csv_logger.addHandler(csv_log_handler)
+
+    # do roll log file
+    need_roll = os.path.isfile(log_path)
+    if need_roll:
+        report_log_handler.doRollover()
+        test_log_handler.doRollover()
+        csv_log_handler.doRollover()
