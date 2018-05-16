41021e18720d7589ff3d08002127b021488c9d7e
Feliciano Long <noahzark@vip.qq.com>
2018-05-16 17:32:19 -0500

    add process support to app

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-import os
 import sys
 import time
 from time import sleep
 
+import multiprocessing
+
 from grader_thread import GraderThread
+from grader_thread import GraderProcess
+
 from grader import Grader
 from model.shared_counter import SharedCounter
 
 from controller.config_file_handler import YamlConfigFileHandler
 
+from init import init_log_file
 from init import report_logger
 
-GRADER_VERSION = '1.2.2'
+GRADER_VERSION = '1.3'
 
 if __name__ == '__main__':
     report_logger.info("QiwuGrader ver {0}".format(GRADER_VERSION))
@@ -44,28 +48,52 @@ if __name__ == '__main__':
         test_config = YamlConfigFileHandler(test_config_file_name)
 
         if test_session == 1:  # single session grade
+            init_log_file()
+
             grader = Grader()
             grader.init(test_config)
             grader.test()
         elif test_session > 1:  # multi session grade
             # calculate thread spawn interval
             spawn_interval = test_length / (test_session * 1.0)
+
+            # determine grader class
+            use_process = False
+            handler_count = test_session
+            session_per_handler = 1
+            Handler_Class = GraderThread
+
+            # use process to speed up grade
+            if spawn_interval < 0.1 and test_session >= 10000:
+                use_process = True
+                handler_count = multiprocessing.cpu_count()
+                session_per_handler = test_session / handler_count
+                Handler_Class = GraderProcess
+
             # count the number of spawned sessions
             session_count = 0
+
             # thread safe success counter
             success_count = SharedCounter()
+
             # process time counter
             process_time = time.time()
+
             # thread group
             threads = []
 
-            report_logger.info("Testing {0} sessions in {1} seconds, interval: {2}".format(test_session, test_length, spawn_interval))
+            if not use_process:
+                init_log_file()
+
+            report_logger.info("Testing {0} sessions in {1} seconds, interval: {2}, using class {3}"
+                               .format(test_session, test_length, spawn_interval, Handler_Class.__name__))
             report_logger.info("Warming up ...")
 
             warm_up_time = time.time()
             # Spawn threads
-            while session_count < test_session:
-                grader_thread = GraderThread(success_count, test_config)
+            while session_count < handler_count:
+                grader_thread = Handler_Class(success_count, test_config,
+                                              loop=session_per_handler, spawn_interval=spawn_interval * handler_count)
                 grader_thread.init()
 
                 threads.append(grader_thread)
@@ -81,7 +109,8 @@ if __name__ == '__main__':
                 # Wait for spawn interval
                 sleep(spawn_interval)
 
-            report_logger.info("{0} sessions started in {1}".format(session_count, time.time() - launch_time))
+            report_logger.info("{0} sessions started in {1}".format(
+                int(session_count * session_per_handler), time.time() - launch_time))
 
             # Wait for all threads to finish
             for grader_thread in threads:
