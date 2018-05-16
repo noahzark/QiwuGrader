7c652ca12391b16ea3938f7e2d287edd8e48bf99
Feliciano Long <noahzark@vip.qq.com>
2018-05-16 16:54:08 -0500

    change a init val name to fix a warning

--- model/shared_counter.py
+++ model/shared_counter.py
@@ -3,8 +3,8 @@ from multiprocessing import Process, Value, Lock
 
 
 class SharedCounter(object):
-    def __init__(self, initval=0):
-        self.val = Value('i', initval)
+    def __init__(self, init_val=0):
+        self.val = Value('i', init_val)
         self.lock = Lock()
 
     def increment(self):
