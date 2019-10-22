358d6a9179644c015d982a60cd00fa6f62208f07
Fangzhou Long <me@imvery.moe>
2019-10-22 20:06:03 +0800

    fix yaml version bug

--- qiwugrader/controller/config_file_handler.py
+++ qiwugrader/controller/config_file_handler.py
@@ -13,7 +13,10 @@ class YamlConfigFileHandler:
         self.filename = filename
 
         with open_config_file(self.filename) as __fr:
-            self.resp_dict = yaml.load(__fr, Loader=yaml.FullLoader)
+            if hasattr(yaml, 'FullLoader'):
+                self.resp_dict = yaml.load(__fr, Loader=yaml.FullLoader)
+            else:
+                self.resp_dict = yaml.load(__fr)
 
     def get_config(self, key, default={}):
         return self.resp_dict[key] if key in self.resp_dict else default
