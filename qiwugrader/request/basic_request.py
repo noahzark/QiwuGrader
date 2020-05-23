dc5257f5b4c0eb50cca1a8a20949b6a737eb57ed
Feliciano Long <me@imvery.moe>
2020-05-23 15:06:18 +0800

    add support to change ednpoint in basic request

--- qiwugrader/request/basic_request.py
+++ qiwugrader/request/basic_request.py
@@ -11,5 +11,6 @@ class BasicRequest(object):
     def __init__(self):
         self.logger = logging.getLogger("RotatingLog")
 
-    def to_uri(self):
-        return '{0}://{1}:{2}{3}'.format(self.protocol, self.host, self.port, self.endpoint)
+    def to_uri(self, endpoint=None):
+        endpoint = endpoint or self.endpoint
+        return '{0}://{1}:{2}{3}'.format(self.protocol, self.host, self.port, endpoint)
